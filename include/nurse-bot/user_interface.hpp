5f059d0fcc8dbb5eccbf13cfb870f64eb2438499
Siddharth Telang <siddharth.telang910@gmail.com>
2021-12-06 02:32:48 -0500

    Add user interface header file

---
+++ include/nurse-bot/user_interface.hpp
@@ -0,0 +1,70 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2021 Sakshi Kakde, Siddharth Telang, Anubhav Paras
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * @file user_interface_node.cpp
+ * @author Sakshi Kakde (sakshi@umd.edu) 
+ * @author Siddharth Telang (stelang@umd.edu)
+ * @author Anubhav Paras (anubhavp@umd.edu)
+ * @brief ROS node for to spawn user interface and publish messages
+ * @version 0.1
+ * @date 2021-11-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef INCLUDE_NURSE_BOT_USER_INTERFACE_HPP_
+#define INCLUDE_NURSE_BOT_USER_INTERFACE_HPP_
+
+#include <ros/ros.h>
+#include <nurse_bot/Task.h>
+#include <nurse_bot/NBTaskAction.h>
+#include <geometry_msgs/Twist.h>
+
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <vector>
+
+#include <nurse-bot/task_publisher.hpp>
+#include <nurse-bot/task_action_client.hpp>
+
+namespace nursebot {
+
+class UserInterface {
+ public:
+    explicit UserInterface(std::shared_ptr<nursebot::TaskActionClient> _task_ac);
+    ~UserInterface();
+    void get_user_input();
+ private:
+    std::shared_ptr<nursebot::TaskActionClient> task_ac;
+    geometry_msgs::Twist entity_position;
+    geometry_msgs::Twist target_position;
+    nurse_bot::NBTaskGoal task_goal;
+    std::vector<float> input{0, 0, 0, 0};
+    int count = 1;
+};
+
+}  //  namespace nursebot
+
+#endif  //  INCLUDE_NURSE_BOT_USER_INTERFACE_HPP_
