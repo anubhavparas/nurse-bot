ecb1aed7c4339224ec0e219d4a5f69c2c4ea973b
Anubhav Paras <anubhavparas94@gmail.com>
2021-12-13 15:11:16 -0500

    [3.9]: added rostest launch files for the new and existing rostests and gtests

---
+++ test/controllers_test_main.cpp
@@ -0,0 +1,45 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2021 Sakshi Kakde, Siddharth Telang, Anubhav Paras
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * @file main.cpp
+ * @author Sakshi Kakde (sakshi@umd.edu) 
+ * @author Siddharth Telang (stelang@umd.edu)
+ * @author Anubhav Paras (anubhavp@umd.edu)
+ * @brief main for controller tests
+ * @version 0.1
+ * @date 2021-11-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "controller_test_node");
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
